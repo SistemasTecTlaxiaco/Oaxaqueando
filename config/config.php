5854fd6580417dbf6aae079adb3ed76830ba34fc
NataliaSantiago <61957975+NataliaSantiago@users.noreply.github.com>
2020-12-07 12:57:04 -0600

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,6 +4,6 @@
     define('HOST', 'ec2-35-153-12-59.compute-1.amazonaws.com');
     define('DB', 'd6g03u8h65dg58');
     define('USER', 'jmaqsngmqtrjhf');
-    define('PASSWORD', '93792bfb920cc5690f4e2c3592d5c0b972ec76b961c90e97a2c5de0a1746d48a');
+    define('PASSWORD', "93792bfb920cc5690f4e2c3592d5c0b972ec76b961c90e97a2c5de0a1746d48a!\"QWE");
     define('CHARSET', 'utf8mb4');
 ?>
