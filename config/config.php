193d9c546d2aa93ac1787b7263dc5b3ce0914c0d
NataliaSantiago <61957975+NataliaSantiago@users.noreply.github.com>
2020-12-07 12:50:08 -0600

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
-    define('URL', 'http://localhost/mvc/');
+    define('URL', 'postgres://jmaqsngmqtrjhf:93792bfb920cc5690f4e2c3592d5c0b972ec76b961c90e97a2c5de0a1746d48a@ec2-35-153-12-59.compute-1.amazonaws.com:5432/d6g03u8h65dg58');
 
-    define('HOST', 'localhost');
-    define('DB', 'mvc');
-    define('USER', 'root');
-    //define('PASSWORD', "123!\"QWE");
+    define('HOST', 'ec2-35-153-12-59.compute-1.amazonaws.com');
+    define('DB', 'd6g03u8h65dg58');
+    define('USER', 'jmaqsngmqtrjhf');
+    define('PASSWORD', "93792bfb920cc5690f4e2c3592d5c0b972ec76b961c90e97a2c5de0a1746d48a");
     define('CHARSET', 'utf8mb4');
 ?>
