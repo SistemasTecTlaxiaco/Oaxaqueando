d7b4fc85f4b6170e7743cc2050fc9a53ace26297
NataliaSantiago <61957975+NataliaSantiago@users.noreply.github.com>
2020-12-17 22:59:05 -0600

    Update database.php

--- libs/database.php
+++ libs/database.php
@@ -10,13 +10,13 @@
             $this->host=constant('HOST');
             $this->db=constant('DB');
             $this->user=constant('USER');
-            $this->password=constant('PASSWORD');
+            //$this->password=constant('PASSWORD');
             $this->charset=constant('CHARSET'); 
         }
         
         function connect(){
             try{
-                $connection = "postgres:host=".$this->host.";dbname=".$this->db.";charset=".$this->charset;
+                $connection = "mysql:host=".$this->host.";dbname=".$this->db.";charset=".$this->charset;
                 $options=[
                     PDO::ATTR_ERRMODE               =>PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_EMULATE_PREPARES      =>false,
