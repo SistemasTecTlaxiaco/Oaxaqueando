287063c3a4b9a736e25d66e81853cbd8eaa8c697
ObedG <65927298+ObedG@users.noreply.github.com>
2021-01-11 02:46:33 -0600

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
-    define('URL', 'https://oaxaqueando1.herokuapp.com/');
-
-    define('HOST', 'ec2-35-153-12-59.compute-1.amazonaws.com');
-    define('PORT', '5432');
+    define('URL', 'postgres://jmaqsngmqtrjhf:93792bfb920cc5690f4e2c3592d5c0b972ec76b961c90e97a2c5de0a1746d48a@ec2-35-153-12-59.compute-1.amazonaws.com:5432/d6g03u8h65dg58');
+    
+    define('HOST', 'ec2-35-153-12-59.compute-1.amazonaws.com:5432');
+   
     define('DB', 'd6g03u8h65dg58');
     define('USER', 'jmaqsngmqtrjhf');
     define('PASSWORD', '93792bfb920cc5690f4e2c3592d5c0b972ec76b961c90e97a2c5de0a1746d48a');
