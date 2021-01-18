cf915c14a9a60d0a48340a5d7e19fd5f39ffa59f
ObedG <65927298+ObedG@users.noreply.github.com>
2021-01-17 20:20:39 -0600

    URL

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-    define('URL', 'http://app-a048003f-c2d8-447d-ae4d-61eaaa1eb11e.cleverapps.io/');
+    define('URL', 'https://oaxaqueando1.herokuapp.com/');
     define('HOST', 'bdyzcfehrcomfhksbhmx-mysql.services.clever-cloud.com');
     define('DB', 'bdyzcfehrcomfhksbhmx');
     define('USER', 'up1bqfux0eibfreq');
